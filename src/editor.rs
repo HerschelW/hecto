0a362f1be0bcf3d924d2c3ff9ab8e6a66434005c
HerschelW <herschelw@gmail.com>
2022-12-14 13:05:13 -0600

    exit without panic

--- src/editor.rs
+++ src/editor.rs
@@ -1,27 +1,32 @@
 use std::fs::DirEntry;
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-pub struct Editor {}
+pub struct Editor {
+    should_quit: bool,
+}
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
         // loop
         loop {
             if let Err(error) = self.process_keypress() {
                 die(&error);
             }
+            if self.should_quit {
+                break;
+            }
         }
     }
     pub fn default() -> Self {
-        Self {}
+        Self { should_quit: false }
     }
-    fn process_keypress(&self) -> Result<(), io::Error> {
+    fn process_keypress(&mut self) -> Result<(), io::Error> {
         let pressed_key = read_key()?;
         match pressed_key {
-            Key::Ctrl('q') => panic!("Program End"),
+            Key::Ctrl('q') => self.should_quit = true,
             _ => (),
         }
         Ok(())
