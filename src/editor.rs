e9eb70f1f57bab5bfe42be454df1f24cac24e73f
HerschelW <herschelw@gmail.com>
2023-02-22 16:01:17 -0600

    adding exit message

--- src/editor.rs
+++ src/editor.rs
@@ -1,4 +1,4 @@
-use std::io::{self, stdout};
+use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
@@ -11,16 +11,26 @@ impl Editor {
         let _stdout = stdout().into_raw_mode().unwrap();
         // loop
         loop {
-            if let Err(error) = self.process_keypress() {
+            if let Err(error) = self.refresh_screen() {
                 die(&error);
             }
             if self.should_quit {
                 break;
             }
+            if let Err(error) = self.process_keypress() {
+                die(&error);
+            }
         }
     }
     pub fn default() -> Self {
         Self { should_quit: false }
+    }
+    fn refresh_screen(&self) -> Result<(), io::Error> {
+        print!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
+        if self.should_quit {
+            println!("Goodbye.\r");
+        }
+        io::stdout().flush()
     }
     fn process_keypress(&mut self) -> Result<(), io::Error> {
         let pressed_key = read_key()?;
@@ -40,5 +50,6 @@ fn read_key() -> Result<Key, io::Error> {
 }
 
 fn die(e: &std::io::Error) {
+    print!("{}", termion::clear::All);
     panic!("{}", e)
 }
